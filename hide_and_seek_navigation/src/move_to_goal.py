a8bccd79a6c569889230e61c1cc285031958f03d
Bălănescu Adrian <balanescuadrian71@gmail.com>
2018-12-12 21:11:55 +0200

    move to goal

--- hide_and_seek_navigation/src/move_to_goal.py
+++ hide_and_seek_navigation/src/move_to_goal.py
@@ -9,6 +9,7 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from actionlib_msgs.msg import GoalStatus
 from geometry_msgs.msg import Pose, Point, Quaternion
 from tf.transformations import quaternion_from_euler
+from hide_and_seek_navigation.msg import listmsg
 
 
 class MoveBaseSeq():
@@ -16,26 +17,27 @@ class MoveBaseSeq():
     def __init__(self):
 
         rospy.init_node('move_base_sequence')
-        points_seq = rospy.get_param('~p_seq')
-        # Only yaw angle required (no ratotions around x and y axes) in deg:
-        yaweulerangles_seq = rospy.get_param('~yea_seq')
-        #List of goal quaternions:
-        quat_seq = list()
-        #List of goal poses:
-        self.pose_seq = list()
-        self.goal_cnt = 0
-        for yawangle in yaweulerangles_seq:
-            #Unpacking the quaternion tuple and passing it as arguments to Quaternion message constructor
-            quat_seq.append(Quaternion(*(quaternion_from_euler(0, 0, yawangle*math.pi/180, axes='sxyz'))))
-        n = 3
-        # Returns a list of lists [[point1], [point2],...[pointn]]
-        points = [points_seq[i:i+n] for i in range(0, len(points_seq), n)]
-        rospy.loginfo(str(points))
-        for point in points:
-            #Exploit n variable to cycle in quat_seq
-            self.pose_seq.append(Pose(Point(*point),quat_seq[n-3]))
-            n += 1
-        #rospy.loginfo(str(self.pose_seq))
+        rospy.Subscriber('move_base_goal', listmsg, self.movebase_client)
+        # points_seq = rospy.get_param('~p_seq')
+        # # Only yaw angle required (no ratotions around x and y axes) in deg:
+        # yaweulerangles_seq = rospy.get_param('~yea_seq')
+        # #List of goal quaternions:
+        # quat_seq = list()
+        # #List of goal poses:
+        # self.pose_seq = list()
+        # self.goal_cnt = 0
+        # for yawangle in yaweulerangles_seq:
+        #     #Unpacking the quaternion tuple and passing it as arguments to Quaternion message constructor
+        #     quat_seq.append(Quaternion(*(quaternion_from_euler(0, 0, yawangle*math.pi/180, axes='sxyz'))))
+        # n = 3
+        # # Returns a list of lists [[point1], [point2],...[pointn]]
+        # points = [points_seq[i:i+n] for i in range(0, len(points_seq), n)]
+        # rospy.loginfo(str(points))
+        # for point in points:
+        #     #Exploit n variable to cycle in quat_seq
+        #     self.pose_seq.append(Pose(Point(*point),quat_seq[n-3]))
+        #     n += 1
+        # #rospy.loginfo(str(self.pose_seq))
         #Create action client
         self.client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
         rospy.loginfo("Waiting for move_base action server...")
@@ -46,7 +48,7 @@ class MoveBaseSeq():
             return
         rospy.loginfo("Connected to move base server")
         rospy.loginfo("Starting goals achievements ...")
-        self.movebase_client()
+        #self.movebase_client()
 
     def active_cb(self):
         rospy.loginfo("Goal pose "+str(self.goal_cnt+1)+" is now being processed by the Action Server...")
@@ -88,8 +90,30 @@ class MoveBaseSeq():
         if status == 8:
             rospy.loginfo("Goal pose "+str(self.goal_cnt)+" received a cancel request before it started executing, successfully cancelled!")
 
-    def movebase_client(self):
-    #for pose in pose_seq:   
+    def movebase_client(self, data):
+    #for pose in pose_seq:
+        data = listmsg()
+        points_seq = [data.x, data.y, 0]
+        # Only yaw angle required (no ratotions around x and y axes) in deg:
+        yaweulerangles_seq = data.yaw
+        #List of goal quaternions:
+        quat_seq = list()
+        #List of goal poses:
+        self.pose_seq = list()
+        self.goal_cnt = 0
+        for yawangle in yaweulerangles_seq:
+            #Unpacking the quaternion tuple and passing it as arguments to Quaternion message constructor
+            quat_seq.append(Quaternion(*(quaternion_from_euler(0, 0, yawangle*math.pi/180, axes='sxyz'))))
+        n = 3
+        # Returns a list of lists [[point1], [point2],...[pointn]]
+        points = [points_seq[i:i+n] for i in range(0, len(points_seq), n)]
+        rospy.loginfo(str(points))
+        for point in points:
+            #Exploit n variable to cycle in quat_seq
+            self.pose_seq.append(Pose(Point(*point),quat_seq[n-3]))
+            n += 1
+        #rospy.loginfo(str(self.pose_seq))
+
         goal = MoveBaseGoal()
         goal.target_pose.header.frame_id = "map"
         goal.target_pose.header.stamp = rospy.Time.now() 
